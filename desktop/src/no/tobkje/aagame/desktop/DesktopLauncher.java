f01dfc884564813a25231330828a1109489076b2
Kjell Arne Sandvold Hellum <kjehe91@gmail.com>
2015-02-16 15:10:03 +0100

    Initial commit with libGDX template

---
+++ desktop/src/no/tobkje/aagame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.tobkje.aagame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.tobkje.aagame.AAGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AAGame(), config);
+	}
+}
